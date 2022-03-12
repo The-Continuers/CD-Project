4ac48facce7f899b7171a0ac58aab13746d1007c
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2022-03-12 18:12:24 +0330

    feat(preprocessor): find define

--- compiler/scanner/preprocessor.py
+++ compiler/scanner/preprocessor.py
@@ -7,8 +7,22 @@ class Preprocessor:
         self.analyzer = analyzer
 
     def _preprocess(self) -> str:
-        # TODO: Sad
+        data = self.analyzer.data
+        clean_data = ""
+        micros = {}
+        for line in data.splitlines():
+            line = line.strip()
+            arr = line.split(" ", 2)
+            if "define" in arr:
+                micros[arr[1]] = arr[2]
+            else:
+                clean_data += line
+        self.analyzer.data = clean_data
+        self.replace_micros(micros)
         return self.analyzer.data
+
+    def replace_micros(self, micros):
+        ...
 
     def preprocess(self):
         self.analyzer.data = self._preprocess()
