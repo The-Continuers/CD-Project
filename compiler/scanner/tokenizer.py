03d6d1bca5a5a17c034c13c9ca58d15a1fd5bf1c
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2022-03-12 16:39:40 +0330

    feat(tokenizer): init tokens again

--- compiler/scanner/tokenizer.py
+++ compiler/scanner/tokenizer.py
@@ -4,16 +4,52 @@ from compiler.scanner import LexicalAnalyzer
 from compiler.scanner.enums import *
 
 # List of token names.This is always required
-tokens = [] + DECAF_RESERVED_KEYWORDS
+tokens = DECAF_RESERVED_KEYWORDS + [
+             'PUNCTUATION',
+             'ID',
+             'INTLITERAL',
+             'DOUBLELITERAL',
+             'STRINGLITERAL',
+             'BOOLEANLITERAL'
+         ]
 
-t_PUNCTUATION = r''  # TODO: Find them
 
-t_ignore = ''  # TODO: Find them
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# todo overlook
+t_PUNCTUATION = r"(<<)|(>>)|(<=)|(>=)|(==)|(!=)|(&&)|(\|\|)|[{}\[\],;()=\-!+*\/<>%]"
+
+t_ID = r"[_a-zA-Z][_a-zA-Z0-9]*"
+
+
+def t_INTLITERAL(t):
+    r"""(0x)?[0-9]+"""
+    t.value = int(t.value)
+    return t
+
+
+def t_DOUBLELITERAL(t):
+    r"""[0-9]+\.[0-9]+"""
+    t.value = float(t.value)
+    return t
+
+
+t_STRINGLITERAL = r"""\"([^"]|(\\(\")?))*\""""
+
+t_BOOLEANLITERAL = r"(true)|(false)"
+
+t_ignore_COMMENT = r"""(\/\/.*)|(\/\*([^\*]|(\*+[^\/\*]))*\*+\/)"""
+
+t_ignore = " \t"
 
 
 def t_error(t):
-    # TODO: Write this
-    pass
+    t.type = 'UNDEFINED_TOKEN'
+    t.lexer.lexpos = t.lexer.lexlen
+    return t
 
 
 class Tokenizer:
