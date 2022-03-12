c71947e25352c9e8e8b5af18a64dbcdeb1e33b88
ashkan <ashkan.khd.q@gmail.com>
2022-03-12 21:10:03 +0330

    fix(preprocessor): fix macro replacer

--- compiler/scanner/preprocessor.py
+++ compiler/scanner/preprocessor.py
@@ -1,10 +1,27 @@
+import random
+
 from compiler.scanner import LexicalAnalyzer
 import re
+
+INSIDE_QUOTATION_REGEX_TEMPLATE = r"('[^'\\]*(?:\\.[^'\\]*)*')|\b{0}\b"
+
 
 class Preprocessor:
 
     def __init__(self, analyzer: LexicalAnalyzer):
         self.analyzer = analyzer
+        self.tof_key = f'ashk{random.randint(1000,9999)}sep{random.randint(1000,9999)}sad'
+
+    def replace_micros(self, data, macros):
+        for key, value in macros.items():
+            data = re.sub(
+                INSIDE_QUOTATION_REGEX_TEMPLATE.format(key),
+                lambda m: m.group(1) if m.group(1) else self.tof_key,
+                data,
+            )
+            data = re.sub(fr'([^A-Za-z0-9_]){self.tof_key}([^A-Za-z0-9_])', f'\g<1>{value}\g<2>', data)
+            data = re.sub(f'{self.tof_key}', f'{key}', data)
+        return data
 
     def _preprocess(self) -> str:
         data = self.analyzer.data
@@ -18,15 +35,7 @@ class Preprocessor:
             else:
                 clean_data += line
                 clean_data += '\n'
-        self.analyzer.data = clean_data
-        self.replace_micros(macros)
-        return self.analyzer.data
-
-    def replace_micros(self, macros):
-        data = self.analyzer.data
-        for key, value in macros.items():
-            data = re.sub(f'([^A-Za-z0-9_])?{key}([^A-Za-z0-9_])?', f'\\1{value}\\2', data)
-        self.analyzer.data = data
+        return self.replace_micros(clean_data, macros)
 
     def preprocess(self):
         self.analyzer.data = self._preprocess()
