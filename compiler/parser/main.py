0a163c02051d6b8ae595c5f98bda3e6c170ba6b5
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2022-05-10 22:48:17 +0430

    refactor(main): sort

--- compiler/parser/main.py
+++ compiler/parser/main.py
@@ -9,6 +9,8 @@ from os.path import isfile, join
 def get_inputs(dir: str) -> Tuple[List[str], List[str]]:
     in_files = [f for f in listdir(dir) if isfile(join(dir, f)) and f.endswith('.in')]
     out_files = [f for f in listdir(dir) if isfile(join(dir, f)) and f.endswith('.out')]
+    in_files.sort()
+    out_files.sort()
     return in_files, out_files
 
 
