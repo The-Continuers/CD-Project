3b353888130a86394e1e2636f03de163bb4b7a00
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2022-05-12 20:25:06 +0430

    feat(core): change main.py for Phase2 Pass

--- compiler/main.py
+++ compiler/main.py
@@ -1,13 +1,15 @@
 from compiler.scanner import LexicalAnalyzer
+from compiler.parser.run_one_test_case import run_test_case
 
 
-def run(input_file_address: str) -> str:
+def run(input_file_address: str) -> bool:
     with open(input_file_address) as input_file:
         data = input_file.read()
+        return True if run_test_case(data) == "OK" else False
 
-    analyzer = LexicalAnalyzer(data)
-    analyzer.analyze()
-    return analyzer.get_all_tokens()
+    # analyzer = LexicalAnalyzer(data)
+    # analyzer.analyze()
+    # return analyzer.get_all_tokens()
 
 
 if __name__ == '__main__':
