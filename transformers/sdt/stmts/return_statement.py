4981163defff5cf23cbf39f19fc9fccac7b85d2e
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2022-08-09 21:09:21 +0430

    test(semantic-4): add return_statement type-checking (void type-checking is ignored)

--- transformers/sdt/stmts/return_statement.py
+++ transformers/sdt/stmts/return_statement.py
@@ -2,14 +2,20 @@ from typing import List, TYPE_CHECKING
 
 from transformers.sdt.stmts import Statement
 from transformers.sdt.stmts.expressions import Expression
+from transformers.sdt.utils import VariableName
 
 if TYPE_CHECKING:
     from code_generation import Context
+    from transformers.types import DecafType
 
 
 class ReturnStatement(Statement):
     def __init__(self, return_expr: Expression):
         self.return_expr = return_expr
+
+    @staticmethod
+    def get_func_name(context: "Context") -> str:
+        return context.current_scope.get_name()
 
     def to_tac(self, context: "Context") -> List[str]:
         """
@@ -17,8 +23,17 @@ class ReturnStatement(Statement):
             therefore, after function return, the assign expression will use that $t0 or $f0,
             considering type-safety issues
         """
+        self.type_check(context=context)
         codes = ["# return expression"]
         if self.return_expr is not None:
             codes += self.return_expr.to_tac(context=context)
-        codes += [f'j end_function_{context.current_scope.get_name()} # Fuck Life']
+        codes += [f'j end_function_{self.get_func_name(context=context)} # Fuck Life']
         return codes
+
+    def type_check(self, context: "Context") -> "DecafType":
+        func_name = self.get_func_name(context=context)
+        func_return_type: "DecafType" = context.current_scope.apply_function(
+            function_name=VariableName(func_name)).return_type
+        if self.return_expr is not None:
+            return self.return_expr.type_checker.check_type(context=context,
+                                                            expected_type=func_return_type)
