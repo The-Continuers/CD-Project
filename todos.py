8258c6fbf0c23a42e7dd0d1b0266e0acbb97b17d
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2022-08-10 15:41:21 +0430

    test(core): add remained issues

--- todos.py
+++ todos.py
@@ -4,13 +4,10 @@ class Todo:
             Print Double In 8bar
         """,
         """
-            Function_Call before Function_def        
-        """,
-        """
-            Arrays
-        """,
-        """
             Array, String Concatenation
+        """,
+        """
+            OOP, Array Syntax Problem
         """,
         """
             OOP
@@ -18,7 +15,4 @@ class Todo:
     ]
 
     test_fixes = {
-        '$ep': [
-            "Semantic Error",
-        ],
     }
