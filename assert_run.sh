3155e742260c74e2ff006d2d330fb74531b9083d
sepehrkianian09 <sepehrkianian09@yahoo.com>
2024-12-05 14:59:54 +0200

    refactor(validation_tests): create a temp_directory variable for assert_run.sh

--- assert_run.sh
+++ assert_run.sh
@@ -1,8 +1,9 @@
 cp -TRv val_tests/ tests/val_tests/
 source .venv/bin/activate
-./run.sh > kos.txt
-final_score="$(grep  -Eo '^[0-9]+$' kos.txt | tail -1)"
-rm kos.txt
+temp_execution_file="kos.txt"
+./run.sh > ${temp_execution_file}
+final_score="$(grep  -Eo '^[0-9]+$' ${temp_execution_file} | tail -1)"
+rm ${temp_execution_file}
 if [ ${final_score} -eq 4 ]
 then
     echo Success
